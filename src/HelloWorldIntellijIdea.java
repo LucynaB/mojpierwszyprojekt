0afad959eb48021906baba972277c03c24b8b4c7
lblaszczyk <lusia22b@wp.pl>
2018-07-02 18:52:50 +0200

    Dodanie pliku HelloWorld

---
+++ src/HelloWorldIntellijIdea.java
@@ -0,0 +1,10 @@
+public class HelloWorldIntellijIdea {
+
+    public static void main(String[] args) {
+        System.out.println("hello1");
+        System.out.println("hello2");
+        System.out.println();
+        System.out.println();
+
+    }
+}
