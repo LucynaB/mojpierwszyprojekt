dae5c0d12a1f301b9a3b695e7b72b969cce0be41
lblaszczyk <lusia22b@wp.pl>
2018-07-02 19:46:38 +0200

    poprawki

--- src/HelloWorldIntellijIdea.java
+++ src/HelloWorldIntellijIdea.java
@@ -1,10 +1,10 @@
 public class HelloWorldIntellijIdea {
 
     public static void main(String[] args) {
-        System.out.println("hello1");
+
         System.out.println("hello2");
         System.out.println("super");
-        System.out.println("działa");
+        System.out.println("działa!!");
 
     }
 }
